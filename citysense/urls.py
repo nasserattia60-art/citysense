3024818950d839bf9503eda0bffdc6ba4db89d2a
nasserattia60-art <nasserattia60@gmail.com>
2026-01-10 23:18:24 +0300

    edit form

--- citysense/urls.py
+++ citysense/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path , include
-from apps.core import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+
 urlpatterns = [
     path("", include("apps.core.urls")),
     path('admin/', admin.site.urls),
@@ -27,5 +31,7 @@ urlpatterns = [
     path("community/", include("apps.community.urls")),
 
 
-]
+] 
+
+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
