b0514b4db838972c74b428bf65eb221b5f4cb729
nasserattia60-art <nasserattia60@gmail.com>
2026-01-18 17:41:48 +0200

    Refactor: Remove obsolete Docker and Railway configuration files; update WSGI settings for better environment handling

--- citysense/wsgi.py
+++ citysense/wsgi.py
@@ -8,11 +8,23 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
-# Use production settings if RENDER or RAILWAY env var is set
-settings_module = 'citysense.settings.prod' if os.getenv('RENDER') or os.getenv('RAILWAY_ENVIRONMENT') else 'citysense.settings.dev'
+# Add project directory to Python path for PythonAnywhere
+project_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if project_dir not in sys.path:
+    sys.path.insert(0, project_dir)
+
+# Determine which settings module to use
+if os.getenv('PYTHONANYWHERE'):
+    settings_module = 'citysense.settings.pythonanywhere'
+elif os.getenv('RENDER') or os.getenv('RAILWAY_ENVIRONMENT'):
+    settings_module = 'citysense.settings.prod'
+else:
+    settings_module = 'citysense.settings.dev'
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
